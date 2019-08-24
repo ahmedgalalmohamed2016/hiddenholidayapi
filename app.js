e29bd2d1c6d05704680e016283d809b259540019
x <x@xs-MacBook-Air.local>
2019-08-24 18:23:13 +0200

    cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Credentials', false);
+  // res.setHeader('Access-Control-Allow-Credentials', false);
 
   next();
 });
